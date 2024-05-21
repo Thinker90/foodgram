a6e30020c2b7f068d02602a77c17f0ab7d1bf517
Thinker90 <100183203+Thinker90@users.noreply.github.com>
2024-05-21 16:40:29 +0500

    create djangoproject and app

--- backend/api/v1/apps.py
+++ backend/api/v1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class V1Config(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'v1'
+    name = 'api'
