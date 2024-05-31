a09c60846c855c022caad18f016420fb653e8fa6
Thinker90 <100183203+Thinker90@users.noreply.github.com>
2024-05-31 12:03:48 +0500

    add apps/models/djoser

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,7 +8,6 @@ router = DefaultRouter()
 #router.register('users', UserViewSet)
 
 urlpatterns = (
-    #path('', include(router.urls)),
     path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('auth/', include('djoser.urls.authtoken')),
 )
