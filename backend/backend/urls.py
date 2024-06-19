5d7e51e0c2b2f720503e6309a3d2c403a5ea7076
Thinker90 <100183203+Thinker90@users.noreply.github.com>
2024-06-19 19:11:00 +0500

    add short link

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from api.views import redirect_short_link
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
-    path('s/<str:short_link>/', redirect_short_link, name='recipe-shortlink'),
+    path('s/', include("api.urls", namespace="api")),
 ]
