5efb433af26e951d97a6c2c2c7c3e16dda377a08
Thinker90 <100183203+Thinker90@users.noreply.github.com>
2024-06-19 19:18:58 +0500

    add short link

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('s/', include("api.urls", namespace="api")),
     path('admin/', admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
-    path('s/', include("api.urls", namespace="api")),
 ]
